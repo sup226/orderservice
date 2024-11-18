97609164c6896a3407136d5aa0cfa2e48c251235
sup226 <rlaalswl1102@naver.com>
2024-11-18 10:35:02 +0900

    github action 정상 동작 확인을 위한 커밋

--- src/main/java/com/playdata/orderservice/OrderserviceBack202410Application.java
+++ src/main/java/com/playdata/orderservice/OrderserviceBack202410Application.java
@@ -2,12 +2,18 @@ package com.playdata.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class OrderserviceBack202410Application {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderserviceBack202410Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "the server is running";
     }
 
 }
